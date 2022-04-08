50c934350a5a2332ee0ed9089170b8a14b554894
haxwithaxe <spam@haxwithaxe.net>
2022-04-08 14:17:01 -0400

    Added code to set the default editor

--- install-tools.sh
+++ install-tools.sh
@@ -4,3 +4,8 @@ set -ex
 
 export DEBIAN_FRONTEND=noninteractive  
 apt install -y -qq screen neovim bash-completion net-tools dnsutils
+
+# Set default editor
+update-alternatives --set vi /usr/bin/nvim
+update-alternatives --set vim /usr/bin/nvim
+update-alternatives --set editor /usr/bin/nvim
